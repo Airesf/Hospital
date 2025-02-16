9a2521c56a64aefb76ba1ec0ccc74f32be581516
Gabriel Carvalho <cttbiel@gmail.com>
2025-02-16 09:00:18 -0300

    Usando alocacao dinamica para incluir

--- main.c
+++ main.c
@@ -2,29 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_MEDICOS 20
-#define MAX_PACIENTES 50
-
-
-//Struct dos doctor
+// Funções para o médico
 typedef struct {
     int crm;
-    char name[50];
+    char nome[50];
     char especialidade[50];
-    int ativo; // 1 - Ativo, 0 - Inativo
 } Medico;
 
-
-
-// Funções para o médico
-Medico medicos[MAX_MEDICOS];
+Medico *medicos = NULL;
 int totalMedicos = 0;
 
 
-void addDoctor(){
-    if(totalMedicos >=MAX_MEDICOS){
-        printf("Numero maximo de médicos atingidos, se você quiser adicionar outro, demita alguém para adicionar este novo.\n");
+void incluirMedico(){
+    medicos = (Medico *)realloc(medicos,(totalMedicos+1) * sizeof(Medico));
+    if (medicos == NULL){
+        printf("Eita, deu erro na hora de alocar a memoria doidho!\n");
         return;
+    }
+
+    printf("\n --- Cadastro de Medico ---");
+    printf("Digite o CRM: ");
+    scanf(" %d", &medicos[totalMedicos].crm);
+    getchar();
+
+    printf("Digite o nome: ");
+    fgets(medicos[totalMedicos].nome, 50, stdin);
+    medicos[totalMedicos].nome[strcspn(medicos[totalMedicos].nome, "\n")] = 0;
+
+    printf("Digite a especialização: ");
+    fgets(medicos[totalMedicos].especialidade, 50, stdin);
+    medicos[totalMedicos].especialidade[strcspn(medicos[totalMedicos].especialidade, "\n")] = 0;
+
+    totalMedicos++;
+    printf("Medico cadastrado com sucesso!\n");
+
     }
     
     printf("\n --- Cadastro de Medico ---\n");
@@ -45,6 +56,8 @@ void addDoctor(){
 
     printf("Medico cadastrado com sucesso no sistema!\n");
 }
+
+
 
 //Listing existing doctors for you my brother
 void listDoctors(){
@@ -80,6 +93,7 @@ void menuMedico(){
         printf("\n-------Menu-------\n");
         printf("1 - Incluir Medico\n");
         printf("2 - Listar Medicos\n");
+        printf("3 - Atualizar Medico\n");
         printf("3 - Excluir Medico\n");
         printf("4 - Sair\n");
         printf("O que voce quer pra hoje meu pratao?: ");
@@ -125,7 +139,6 @@ typedef struct {
     char adress[100]; //Bro's adress
     int telefone;
     char sexo[50];
-    int taVivo; // 1 - Vivo; 0 - Morto;
 } Paciente;
 
 void addPaciente() {
@@ -153,7 +166,7 @@ void menuPacientes() {
 //Inicio das funções para consulta
 
 typedef struct {
-    
+
     int consulta;
     int horario;
     int data;
@@ -169,12 +182,25 @@ void menuConsultas(){
 
 //Fim das funções de consulta
 
+
+
+//Inicio Função menuRelatorio
+void menuRelatorio(){
+
+    //Dentro da opção relatórios o usuário poderá escolher entre:
+    //Lista de consultas agendadas para o paciente.
+    //Lista de consultas agendadas para o médico.
+    //Lista de pacientes por especialidade.
+
+}
+//Fim Função menuRelatorio
+
 int main () {
 
     int opcoes;
-    printf("Olá, seja bem vindo(a) ao sistema de gerenciamnento de consultas médicas!\n");
+    printf("Ola, seja bem vindo(a) ao sistema de gerenciamnento de consultas medicas!\n");
     printf("Developed by cttbiel and aires.\n");
-    printf("Por favor, escolha uma das opções abaixo: \n");
+    printf("Por favor, escolha uma das opcoes abaixo: \n");
 
     do
     {
@@ -189,6 +215,7 @@ int main () {
     case 1:
         menuMedico();
         break;
+
     case 2:
         menuPacientes();
         break;
@@ -196,14 +223,19 @@ int main () {
     case 3:
         menuConsultas();
         break;
+
     case 4:
-        printf("Fechando aplicação...\n");
+        menuRelatorio();
+        break;
+
+    case 5:
+        printf("Fechando aplicacao...\n");
         break;
     
     default:
         printf("Opcao invalida patrao, joga ota ai pa nois\n");
         break;
     }
-    } while (opcoes != 4);
+    } while (opcoes != 5);
     return 0;
 }
